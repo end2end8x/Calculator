84ff25f118f8ce4ed0f3418d59089200d30a7773
Tran Danh Tuan <end2end8x@gmail.com>
2019-12-24 14:23:56 +0700

    fix eslint

--- src/reducer.js
+++ src/reducer.js
@@ -6,43 +6,44 @@ import {
 } from "./constants";
 
 const rootReducer = (state = initialState, action) => {
+  const operator = ["+", "-", "*", "/"];
+  const regex = /^\d+(\.\d+)?([+\-*\/]\d+(\.\d+)?)*$/gm;
+  let { payload } = action;
+  const { expression } = state;
+  const indexPayload = operator.indexOf(payload);
+  const lastOp = expression.slice(-1);
+  const indexlastOp = operator.indexOf(lastOp);
+  if (
+    payload === "." &&
+    lastOp !== "." &&
+    (indexlastOp != -1 || expression === "0")
+  ) {
+    payload = `0${payload}`;
+  }
+  const newExpression = expression + payload;
+
   switch (action.type) {
     case CLEAR_RESULT:
       return initialState;
     case CALCULATE_RESULT: {
       let result;
       try {
-        const temp = eval(state.expression);
+        const temp = eval(expression);
         result =
           temp === parseInt(temp)
             ? temp
             : Math.round(temp * 100000000) / 100000000;
       } catch (e) {
-      } finally {
-        if (!result || typeof +result !== "number") result = 0;
-        return {
-          ...state,
-          expression: "0",
-          result
-        };
+        console.log(`CALCULATE_RESULT Exception ${e}`);
       }
+      if (!result || typeof +result !== "number") result = 0;
+      return {
+        ...state,
+        expression: "0",
+        result
+      };
     }
     case SAVE_EXPRESSION:
-      const operator = ["+", "-", "*", "/"];
-      const regex = /^\d+(\.\d+)?([+\-*\/]\d+(\.\d+)?)*$/gm;
-      let { payload } = action;
-      const { expression } = state;
-      const indexPayload = operator.indexOf(payload);
-      const lastOp = expression.slice(-1);
-      const indexlastOp = operator.indexOf(lastOp);
-      if (
-        payload === "." &&
-        lastOp !== "." &&
-        (indexlastOp != -1 || expression === "0")
-      ) {
-        payload = `0${payload}`;
-      }
-      const newExpression = expression + payload;
       if (payload === ".") {
         if (lastOp === ".") return state;
         if (!`${newExpression}0`.match(regex)) return state;
