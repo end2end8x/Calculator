031b0b8ebfdbbbafa402c627c6685064d37cc96e
Tran Danh Tuan <end2end8x@gmail.com>
2019-12-24 10:32:29 +0700

    regex

--- src/reducer.js
+++ src/reducer.js
@@ -30,20 +30,38 @@ const rootReducer = (state = initialState, action) => {
     }
     case SAVE_EXPRESSION:
       const operator = ["+", "-", "*", "/"];
-      const indexPayload = operator.indexOf(action.payload);
-      const indexOperator = operator.indexOf(
-        state.expression[state.expression.length - 1]
-      );
-      if (state.expression === "0" && indexPayload != -1) return state;
+      const regex = /^\d+(\.\d+)?([+\-*\/]\d+(\.\d+)?)*$/gm;
+      let { payload } = action;
+      const { expression } = state;
+      const indexPayload = operator.indexOf(payload);
+      const lastOp = expression.slice(-1);
+      const indexlastOp = operator.indexOf(lastOp);
+      if (
+        payload === "." &&
+        lastOp !== "." &&
+        (indexlastOp != -1 || expression == 0)
+      ) {
+        payload = `0${payload}`;
+      }
+      const newExpression = expression + payload;
+      if (payload === ".") {
+        if (lastOp == ".") return state;
+        if (newExpression.match(regex)) {
+          console.log(`MATCH ${newExpression}`);
+        } else {
+          console.log(`NOT Match ${newExpression}`);
+        }
+      }
+      if (expression === "0" && indexPayload != -1) return state;
+      // console.log(`${JSON.stringify(state)} ${JSON.stringify(action)}`);
       const newState = {
         ...state,
         expression:
-          state.expression === "0"
-            ? action.payload
-            : indexPayload != -1 && indexOperator != -1
-            ? state.expression.substr(0, state.expression.length - 1) +
-              action.payload
-            : state.expression + action.payload
+          expression === "0"
+            ? payload
+            : indexPayload != -1 && indexlastOp != -1
+            ? expression.substr(0, expression.length - 1) + payload
+            : newExpression
       };
       return newState;
     default:
